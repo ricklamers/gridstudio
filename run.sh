9181c9dde1f6e1d3174d5ece4f7a8b7d35635f57
Rick Lamers <ricklamers@gmail.com>
2019-08-01 21:49:13 +0200

    fixed mac sed incompat. by not running it on mac

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-WIN_PWD=$PWD
-WIN_PWD=$(echo $WIN_PWD | sed -r 's/[/]+/\\/g')
-WIN_PWD=$(echo $WIN_PWD | sed -r 's/\\([a-z])\\+/\U\1:\\/g')
-
 if [[ "$OSTYPE" == "msys" ]]; then
+
 	if [ ! "$(docker ps -a | grep gridstudio)" ]; then
+
+		WIN_PWD=$PWD
+		WIN_PWD=$(echo $WIN_PWD | sed -r 's/[/]+/\\/g')
+		WIN_PWD=$(echo $WIN_PWD | sed -r 's/\\([a-z])\\+/\U\1:\\/g')
+
 		docker run --name=gridstudio --rm=false -v $WIN_PWD\\grid-app:/home/source -v $WIN_PWD\\grid-app\\proxy\\userdata:/home/userdata -p 8080:8080 -p 4430:4430 ricklamers/gridstudio:release
 	else
 		docker start gridstudio
