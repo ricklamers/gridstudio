0813a7258a66bf249c7b00aaa393e06b654f073b
Rick Lamers <ricklamers@gmail.com>
2019-08-07 15:52:46 +0200

    Add warning message when gridstudio container is found

--- run.sh
+++ run.sh
@@ -10,12 +10,14 @@ if [[ "$OSTYPE" == "msys" ]]; then
 
 		docker run --name=gridstudio --rm=false -v $WIN_PWD\\grid-app:/home/source -v $WIN_PWD\\grid-app\\proxy\\userdata:/home/userdata -p 8080:8080 -p 4430:4430 ricklamers/gridstudio:release
 	else
+		echo "gridstudio container detected - starting container - want to do a full restart? Run destroy.sh first."
 		docker start gridstudio
 	fi
 else
 	if [ ! "$(docker ps -a | grep gridstudio)" ]; then
 		docker run --name=gridstudio --rm=false -v $PWD/grid-app:/home/source -v $PWD/grid-app/proxy/userdata:/home/userdata -p 8080:8080 -p 4430:4430 ricklamers/gridstudio:release
 	else
+		echo "gridstudio container detected - starting container - want to do a full restart? Run destroy.sh first."
 		docker start gridstudio
 	fi
 fi
